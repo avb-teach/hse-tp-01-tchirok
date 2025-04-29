5ea2a8c4d4f54cee0f86312592c8e506059789f4
tchirok <zxcuqqe@gmail.com>
2025-04-29 12:43:02 +0300

    2 testa

--- collect_files.sh
+++ collect_files.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+if [ "$#" -ne 2 ]; then
+  echo "Usage: collect_files.sh input_directory output_directory"
+  exit 1
+fi
+
 mkdir -p "$2"
 
 find "$1" -type f -print0  | while IFS=  read -r -d '' file; do
-  cp "$file" "${2}"
+  cp "$file" "$2"
 done
+
